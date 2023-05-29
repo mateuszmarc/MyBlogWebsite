c0ab8dcd572fcb655baad5f1117f4a47e628426c
Mateusz Marcykiewicz <mmarcykiewicz@gmail.com>
2023-05-29 17:15:11 +0000

    add register functionality to main,py and reate register.html file

--- main.py
+++ main.py
@@ -1,32 +1,45 @@
 import datetime
+import sqlite3
 
-from flask import Flask, render_template, redirect, url_for, request
+from wtforms.ext import sqlalchemy
+
+from my_forms import CreatePostForm, EditPostForm, RegisterForm
+from flask import Flask, render_template, redirect, url_for, request, flash
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired, URL
-from flask_ckeditor import CKEditor, CKEditorField
-
+from flask_ckeditor import CKEditor
+from flask_login import login_user, LoginManager, current_user, logout_user,\
+    UserMixin
+from flask_bcrypt import Bcrypt
+import os
+import dotenv
+from sqlalchemy.exc import OperationalError
 
 ## Delete this code:
 # import requests
 # posts = requests.get("https://api.npoint.io/43644ec4f0013682fc0d").json()
 
+dotenv.load_dotenv('.env')
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.getenv('secret_key')
 ckeditor = CKEditor(app)
 Bootstrap(app)
-
-
+login_manager = LoginManager()
+login_manager.init_app(app)
+bcrypt = Bcrypt(app)
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
 ##CONFIGURE TABLE
 class BlogPost(db.Model):
+    __tablename__ = 'blog_post'
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(250), unique=True, nullable=False)
     subtitle = db.Column(db.String(250), nullable=False)
@@ -44,39 +57,27 @@ class BlogPost(db.Model):
         self.img_url = img_url
 
 
-##WTForm
-class CreatePostForm(FlaskForm):
-    title = StringField("Blog Post Title", validators=[DataRequired()])
-    subtitle = StringField("Subtitle", validators=[DataRequired()])
-    author = StringField("Your Name", validators=[DataRequired()])
-    img_url = StringField("Blog Image URL", validators=[DataRequired(), URL()])
-    body = CKEditorField("Blog Content", validators=[DataRequired()])
-    submit = SubmitField("Submit Post")
+class User(UserMixin, db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(250), unique=True, nullable=False)
+    email = db.Column(db.String(250), unique=True, nullable=False)
+    password = db.Column(db.String(1000), nullable=False)
 
 
-class EditPostForm(FlaskForm):
-    title = StringField("Blog Post Title", validators=[DataRequired()])
-    subtitle = StringField("Subtitle", validators=[DataRequired()])
-    author = StringField("Your Name", validators=[DataRequired()])
-    img_url = StringField("Blog Image URL", validators=[DataRequired(), URL()])
-    body = CKEditorField("Blog Content", validators=[DataRequired()])
-    submit = SubmitField("Submit Post")
+def check_database_table(table):
+    try:
+        all_records = db.session.query(table).all()
+    except OperationalError:
+        with app.app_context():
+            db.create_all()
 
 
 @app.route('/')
 def get_all_posts():
+    check_database_table(BlogPost)
     posts = db.session.query(BlogPost).all()
     return render_template("index.html", all_posts=posts)
-
-
-# @app.route("/post/<int:index>")
-# def show_post(index):
-#     posts = db.session.query(BlogPost).all()
-#     requested_post = None
-#     for blog_post in posts:
-#         if blog_post.id == index:
-#             requested_post = blog_post
-#     return render_template("post.html", post=requested_post)
 
 
 @app.route("/about")
@@ -95,7 +96,8 @@ def add_post():
     if request.method == 'GET':
         return render_template('make-post.html', form=form)
     else:
-        if form.validate_on_submit():
+        check_database_table(User)
+        if User.query.filter_by(email=form.email.data):
             new_post = BlogPost(
                 title=form.title.data,
                 subtitle=form.subtitle.data,
@@ -160,5 +162,36 @@ def delete_post():
     return redirect(url_for('get_all_posts'))
 
 
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            new_user_email = User.query.filter_by(email=form.email.data).first()
+            if new_user_email:
+                flash('This email is already registered.')
+                redirect(url_for('login'))
+            else:
+                new_user = User()
+                new_user.username = form.username.data
+                new_user.email = form.email.data
+                new_user.password = \
+                    bcrypt.generate_password_hash(password=form.password.data,
+                                                  rounds=10)
+                with app.app_context():
+                    db.session.add(new_user)
+                    db.session.commit()
+                    return redirect(url_for('get_all_posts'))
+    return render_template("register.html", form=form)
+
+
+@app.route('/login')
+def login():
+    return render_template("login.html")
+
+
+@app.route('/logout')
+def logout():
+    return redirect(url_for('get_all_posts'))
 if __name__ == "__main__":
     app.run(debug=True)
